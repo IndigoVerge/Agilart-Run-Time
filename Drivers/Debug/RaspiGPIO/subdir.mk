f4a2be8e4d6a3a2e81eaab1e71e4dd379e89b64a
Agilart Team <b.rakova@gmail.com>
2012-12-07 16:03:06 +0200

    Updated the make file

--- Drivers/Debug/RaspiGPIO/subdir.mk
+++ Drivers/Debug/RaspiGPIO/subdir.mk
@@ -34,7 +34,7 @@ RaspiGPIO/%.o: ../RaspiGPIO/%.cpp
 RaspiGPIO/%.o: ../RaspiGPIO/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: Sourcery CodeBench C Compiler'
-	$(CC_PREFIX)-gcc -I"../../Common" -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -fcommon -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" $(CFLAGS) -o "$@" "$<"
+	$(CC_PREFIX)gcc -I"../../Common" -O0 -g3 -Wall -c -fmessage-length=0 -fPIC -fcommon -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" $(CFLAGS) -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
